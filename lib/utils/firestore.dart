065111505684cb950ba7166ec7176bcccf8b07e6
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2023-10-21 19:53:51 +0300

    prepare firestore read & write

---
+++ lib/utils/firestore.dart
@@ -0,0 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirebaseFireStore {
+  late CollectionReference collectioName;
+
+  Future<void> initialize() async {
+    collectioName = FirebaseFirestore.instance.collection("chat-me-sara");
+  }
+
+  Stream<QuerySnapshot<Object?>> read() {
+    return collectioName.snapshots();
+  }
+
+  Future<DocumentReference<Object?>> write(Map<String, dynamic> message) {
+    return collectioName.add(message);
+  }
+}
